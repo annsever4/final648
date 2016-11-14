aba273ece879f4964b3f9f8f02e2dbffc2c6de90
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-13 17:54:25 -0800

    added new core class request in order to abstract superglobal calls and make the code easier to read. shout out to panique/huge

---
+++ team07/application/core/request.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: login
+ * Date: 11/13/16
+ * Time: 3:10 PM
+ * INSPIRED BY panique/huge
+ */
+
+//Request CLASS abstracts access to global super variables
+class Request
+{
+
+    public static function post($key, $clean = false)
+    {
+        if(isset($_POST[$key])){
+            return ($clean) ? trim(strip_tags($_POST[$key])) : $_POST[$key];
+        }
+    }
+
+    public static function postCheckbox($key)
+    {
+        return isset($_POST[$key]) ? 1 : NULL;
+    }
+
+    public static function get($key)
+    {
+        if (isset($_GET[$key])) {
+            return $_GET[$key];
+        }
+    }
+
+    public static function cookie($key)
+    {
+        if (isset($_COOKIE[$key])) {
+            return $_COOKIE[$key];
+        }
+    }
+}
