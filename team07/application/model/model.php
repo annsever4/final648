60e54461bef990e7057418190de0e4c93dfab418
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-13 21:26:00 -0800

    added the sort by drop down back to the view so we can add functionality to it tomorrrw. Also changed the getListing() function into the model back to  my version of it so that the results can be sorted

--- team07/application/model/model.php
+++ team07/application/model/model.php
@@ -17,23 +17,17 @@ class Model
     /**
      * Get all songs from database
      */
-    public function getListing($key)
+    public function getListing($key, $order)
     {
 	
-	/***
+
         $sql = "SELECT listings.id, listings.address, listings.price, images.image FROM listings INNER JOIN images ON listings.image_id = images.id WHERE listings.address LIKE '%".$key."%'".
         " ORDER BY listings.".$order;
         $query = $this->db->prepare($sql);
-	*/
-	$sql = "SELECT listings.id, listings.address, listings.price, images.image FROM listings INNER JOIN images ON listings.id=images.id  WHERE address LIKE '%".$key."%'";
-        $query = $this->db->prepare($sql);
+        $query -> execute();
 
-        $query->execute();
 
-        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
-        // core/controller.php! If you prefer to get an associative array as the result, then do
-        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
-        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
+
         return $query->fetchAll();
     }
 
