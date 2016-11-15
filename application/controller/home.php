ffc4295c7d635d22b0fbead72a92a1f950f6b706
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-15 14:36:17 -0800

    restructured project

--- application/controller/home.php
+++ application/controller/home.php
@@ -14,12 +14,23 @@ class Home extends Controller
      * PAGE: index
      * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
      */
-    public function index()
+    public function index($xx=null)
     {
+        $apartments = $xx;
+
         // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/index.php';
         require APP . 'view/_templates/footer.php';
+    }
+
+    public function searchListing()
+    {
+
+        if (isset($_POST["key"])) { //do Search
+            $apartments = $this->model->getListing(Request::post("key"), Request::post('slt_sort_by'));
+            $this->index($apartments);
+        }
     }
 
 }
