4c98a0d99ce4878b0ac305d7e58e5e64a5b506ed
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-16 14:56:16 -0800

    fixing bugs

--- team07/application/controller/login_page.php
+++ team07/application/controller/login_page.php
@@ -19,25 +19,22 @@ class Login_page extends Controller
         $user_email = strip_tags(Request::post('user_email'));
         $user_password = strip_tags(Request::post('user_password'));
 
+        if ($this->model->emailAlreadyExists($user_email)) {
         $hash_password = $this->model->getPasswordHash($user_email);
-
-        $password_verification_result = password_verify($user_password,$hash_password->password_hash);
-
-        if($password_verification_result) {
-            //Start the session
-            session_start();
-            $_SESSION['user'] = $user_email;
-            $_SESSION['name'] = $this->model->getUsersName($user_email);
-            $_SESSION['phone_number'] = $this->model->getUserPhoneNumber($user_email);
-            $_SESSION['logged_in'] = 1;
-        }
-
-
-            if (isset($_SESSION['logged_in'])) {
+        $password_verification_result = password_verify($user_password, $hash_password->password_hash);
+            if($password_verification_result) {
+                //Start the session
+                session_start();
+                $_SESSION['user'] = $user_email;
+                $_SESSION['name'] = $this->model->getUsersName($user_email);
+                $_SESSION['phone_number'] = $this->model->getUserPhoneNumber($user_email);
+                $_SESSION['logged_in'] = true;
                 header('location: ' .URL.'proto/index');
-            }else{
-                //don't do anything
             }
+        } else {
+             //reloads page so user can try to log in again <invalid email>
+             header('location: '.URL.'login_page/index');
         }
 
+    }
 }
