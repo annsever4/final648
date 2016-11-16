a38f36471eabaf44093facd1a577ccb55978a33c
Anna Caitlin-Marie Sever <annsever@mail.sfsu.edu>
2016-11-16 01:25:29 +0000

    initial registration page to reflect mockups

--- team07/application/view/registration/index.php
+++ team07/application/view/registration/index.php
@@ -7,15 +7,29 @@
 
 
 <form  action="<?php echo URL; ?>registration/registerNewUser" method="post">
-    <div class="form-group" style="color:#FFFFFF">
+
+ <!--  <form class="form-inline"> --> 
+<form class= "form-inline">
+<div class="form-group" style="color:#ffffff; max-width: 30%;">
+<label for="first_name">First Name</label>
+<input type="text" class="form-control" id="first_name" placeholder="Jane">
+</div>
+<div class="form-group" style="color:#ffffff; max-width:30%;">
+<label for="last_name">Last Name</label>
+<input type="text" class="form-control" id="last_name" placeholder="Doe">
+</div>
+</form>
+  <!-- </form> -->
+
+    <div class="form-group" style="color:#FFFFFF; max-width: 35%;">
         <label for="exampleInputEmail1">Email address</label>
         <input type="email" name="user_email" class="form-control" id="exampleInputEmail1" placeholder="Email">
     </div>
-    <div class="form-group" style="color:#FFFFFF">
+    <div class="form-group" style="color:#FFFFFF; max-width:30%;">
         <label for="exampleInputPassword1">Password</label>
         <input type="password" name="user_password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
-    <div class="form-group" style="color:#FFFFFF">
+    <div class="form-group" style="color:#FFFFFF; max-width:30%;">
         <label for="exampleInputPassword2">Retype Password</label>
         <input type="password" name="user_password_repeat" class="form-control" id="exampleInputPassword2" placeholder="Password">
     </div>
