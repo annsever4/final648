5cdc513600071b7993eb45ea6e92e1973ef92ee8
benson <hzheng@mail.sfsu.edu>
2016-11-17 22:31:32 +0000

    offset for resultsfixed

--- team07/application/view/proto/results.php
+++ team07/application/view/proto/results.php
@@ -14,12 +14,13 @@
         </select>
     </form>
 
-
+  <!-- 
     <br /><b><u>NOTE</u>!</b> We only have 2 apartments in DB,
     <br />search by any character from its addresses:<br />
     <br />566 46th Ave
     <br />362 43rd Ave
     <br />I am logged in
+  //-->
 
 </div>
 
@@ -32,24 +33,31 @@
     <div class="box">
     <h3>Result</h3>
 
-    <table>
-    <thead style="background-color: #ddd; font-weight: bold;">
-    <tr>
-        <td>Id</td>
-        <td>Address</td>
-        <td>Price</td>
-        <td>Pictures</td>
+    <div class="row">
 
-    </tr>
-    </thead>
-    <tbody>
+    <div class="col-md-6 col-md-offset-6">
 
 
+	<!-- ===================== START OF TABLE ================== -->
+	    <table>
+	    <thead style="background-color: #ddd; font-weight: bold;">
+	    <!-- ============= TABLE ROW ============= -->
+ 	    <tr>
+		<td>Id</td>
+		<td>Address</td>
+		<td>Price</td>
+		<td>Pictures</td>
+    	    </tr>
+	    <!-- ============= END TABLEROW ======== -->
 
+            </thead>
+            <tbody>
+    </div>
 
+    </div>
+    
 
     <?php foreach ($apartments as $apartment) { ?>
-
         <tr>
             <td><?php if (isset($apartment->id)) echo htmlspecialchars($apartment->id, ENT_QUOTES, 'UTF-8'); ?></td>
             <td><?php if (isset($apartment->address)) echo htmlspecialchars($apartment->address, ENT_QUOTES, 'UTF-8'); ?> </td>
@@ -57,14 +65,16 @@
             <td height="200" width="200">
                 <?php if (isset($apartment->image)) { ?>
                     <?php echo '<img src="data:image/jpeg;base64, '.base64_encode($apartment->image).
-                        '" height="150" width="150"/>' ?>
+                        '"max-height="200" width="200"/>' ?>
                 <?php } ?>
-            </td></div>
+            </td>
+    
         </tr>
     <?php } ?>
     </tbody>
+
+    
     </table>
     </div>
-    </div>
-
+    <!--================= END OF TABLE ================== > 
 <?php } ?>
