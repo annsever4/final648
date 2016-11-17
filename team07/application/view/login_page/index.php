07bb3fb3a4e75e356ccffef557809b213d1ae114
benson <hzheng@mail.sfsu.edu>
2016-11-17 21:57:47 +0000

    login registration update

--- team07/application/view/login_page/index.php
+++ team07/application/view/login_page/index.php
@@ -10,14 +10,14 @@
 <form action="<?php echo URL; ?> login_page/loginRegisteredUser" method="post">
 
   <div class="row">
-  <div class="col-md-4 col-md-offset-4" style="color:#FFFFFF">
+  <div class="col-md-4 col-md-offset-4 form-group" style="color:#FFFFFF">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" name="email_input" class="form-control" id="exampleInputEmail1" placeholder="Email">
   </div>
   </div>
 
   <div class="row">
-  <div class="col-md-4 col-md-offset-4" style="color:#FFFFFF">
+  <div class="col-md-4 col-md-offset-4 form-group" style="color:#FFFFFF">
     <label for="exampleInputPassword1">Password</label>
     <input type="password" name="password_input" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
--- team07/application/view/registration/index.php
+++ team07/application/view/registration/index.php
@@ -13,12 +13,12 @@
 
 
 <div class="row">
-    <div class="col-md-4 col-md-offset-4" style="color:#ffffff; max-width:20%;">
+    <div class="col-md-4 col-md-offset-4 form-group" style="color:#ffffff; max-width:20%;">
     <label for="first_name">First Name</label>
     <input type="text" name="user_first_name" class="form-control" id="first_name" placeholder="Jane">
     </div>
 
-    <div class="col-md-4 " style="color:#ffffff; max-width:20%;">
+    <div class="col-md-4 form-group" style="color:#ffffff; max-width:20%;">
     <label for="last_name">Last Name</label>
     <input type="text" name="user_last_name" class="form-control" id="last_name" placeholder="Doe">
     </div>
@@ -26,21 +26,21 @@
 
 
     <div class="row">
-    <div class="col-md-4 col-md-offset-4" style="color:#ffffff; max-width:50%;">
+    <div class="col-md-4 col-md-offset-4 form-group" style="color:#ffffff; max-width:50%;">
     <label for="phone_number">Phone Number #</label>
     <input type="text" name="user_phone_number" class="form-control" id="phone_number" placeholder="(##) ###-####">
     </div>
     </div>
 
     <div class="row">
-    <div class="col-md-4 col-md-offset-4" style="color:#FFFFFF; max-width: 30%;">
+    <div class="col-md-4 col-md-offset-4 form-group" style="color:#FFFFFF; max-width: 30%;">
         <label for="exampleInputEmail1">Email address</label>
         <input type="email" name="user_email" class="form-control" id="exampleInputEmail1" placeholder="Email">
     </div>
     </div>
 
     <div class="row">
-    <div class="col-md-4 col-md-offset-4" style="color:#FFFFFF; max-width:30%;">
+    <div class="col-md-4 col-md-offset-4 form-group" style="color:#FFFFFF; max-width:30%;">
         <label for="exampleInputPassword1">Password</label>
         <input type="password" name="user_password" class="form-control" id="exampleInputPassword1" placeholder="Password">
     </div>
@@ -48,7 +48,7 @@
 
 
     <div class="row">
-    <div class="col-md-4 col-md-offset-4" style="color:#FFFFFF; max-width:30%;">
+    <div class="col-md-4 col-md-offset-4 form-group" style="color:#FFFFFF; max-width:30%;">
         <label for="exampleInputPassword2">Retype Password</label>
         <input type="password" name="user_password_repeat" class="form-control" id="exampleInputPassword2" placeholder="Password">
     </div>
