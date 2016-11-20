6a1a4632dc05a1db6e604d08ca3c3ef7642599cb
benson <hzheng@mail.sfsu.edu>
2016-11-20 05:21:26 +0000

    IDK anymore , grid systesms fixed

--- team07/application/view/proto/results.php
+++ team07/application/view/proto/results.php
@@ -76,8 +76,8 @@
 	   
 	<!-- START LISTINGS DISPLAY --> 
     	<?php foreach ($apartments as $apartment) { ?>
-        <div class="row" style="margin:5px;">
-	    <div class="col-xs-6 col-sm-3" style="padding:2px">
+        <div class="row" id="fancy-border">
+	    <div class="col-xs-6 col-sm-3" style="padding:5px">
                 <?php if (isset($apartment->image)) { ?>
                     <?php echo '<img src="data:image/jpeg;base64, '.base64_encode($apartment->image).
                         '"max-height="300px" max-width="300px"/>' ?>
@@ -85,15 +85,23 @@
             </div>
 	        <div class="col-xs-9" id="pill-shape"><h5>TITLE</h5></div>   
             <!-- <div style="color:blue;"><?php if (isset($apartment->id)) echo htmlspecialchars($apartment->id, ENT_QUOTES, 'UTF-8'); ?></div> --> 
-		<div class="col-xs-6 col-sm-3" id="pill-shape"><?php if (isset($apartment->address)) echo htmlspecialchars($apartment->address, ENT_QUOTES, 'UTF-8'); ?> </div>
-	        <div class="col-xs-6 col-sm-3"  id="pill-shape">$<?php if (isset($apartment->price)) echo htmlspecialchars($apartment->price, ENT_QUOTES, 'UTF-8'); ?> </div>
-		<div class="col-xs-6 col-sm-3" id="pill-shape">SQURE FEET</div>	    
+		<div class="col-xs-6 col-sm-2" id="pill-shape"><?php if (isset($apartment->address)) echo htmlspecialchars($apartment->address, ENT_QUOTES, 'UTF-8'); ?> </div>
+		<div class="col-sm-1"></div>
+		<div class="col-xs-6 col-sm-2"  id="pill-shape">$<?php if (isset($apartment->price)) echo htmlspecialchars($apartment->price, ENT_QUOTES, 'UTF-8'); ?> </div>
+		<div class="col-sm-1"></div>
+		<div class="col-xs-6 col-sm-2" id="pill-shape">SQURE FEET</div>	    
+		<div class="col-sm-1"></div>
 
 	    	<div class="row">
-			<div class="col-xs-6 col-sm-3" id="pill-shape"> BEDS </div>
-			<div class="col-xs-6 col-sm-3" id="pill-shape"> BATHROOM </div>
-			<div class="col-xs-6 col-sm-3"id="pill-shape"> EMAIL</div>
-			<div class="col-xs-6 col-sm-3"id="pill-shape"> PHONE </div>
+			<div class="col-sm-1"></div>
+			<div class="col-xs-6 col-sm-2" id="pill-shape"> BEDS </div>
+			<div class="col-sm-1"></div>
+			<div class="col-xs-6 col-sm-2"id="pill-shape"> BATHROOM </div>
+			
+			<div class="col-xs-7"></div>	
+			<div class="col-xs-6 col-sm-2"id="pill-shape"> EMAIL</div>
+			<div class="col-sm-1"></div>
+			<div class="col-xs-6 col-sm-2"id="pill-shape"> PHONE </div>
 		</div>   	
         </div>
 	<!-- END OF LISTINGS DISPLAY -->
