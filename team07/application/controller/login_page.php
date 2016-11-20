66894b51d62635903bbc034d6c49538e6463dce1
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-20 15:10:15 -0800

    fixing session across pages

--- team07/application/controller/login_page.php
+++ team07/application/controller/login_page.php
@@ -36,7 +36,7 @@ class Login_page extends Controller
             $_SESSION['phone_number'] = $this->model->getMemberUserPhoneNumber($user_email);
             $_SESSION['logged_in'] = true;
 
-//            $_SESSION['user'] = $_POST['user_email'];
+
 
 
 
--- team07/application/view/proto/index.php
+++ team07/application/view/proto/index.php
@@ -53,11 +53,7 @@
 
 <p>
     <?php if(session_start()) echo htmlspecialchars($_SESSION['user'], ENT_QUOTES, 'UTF-8'); ?>
-<?php if(isset($_SESSION['user'])) echo htmlspecialchars($_SESSION['user'], ENT_QUOTES, 'UTF-8'); ?>
 
-<?php if(isset($_SESSION['name'])) echo htmlspecialchars($_SESSION['name'], ENT_QUOTES, 'UTF-8'); ?>
-
-<?php if(isset($_SESSION['phone_number'])) echo  $_SESSION['name']?>
 </p>
 <!-- Output  Test -->
 
