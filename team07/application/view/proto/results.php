cbd01bcbb635abacea5d2e710c4f2db4e7577bbe
benson <hzheng@mail.sfsu.edu>
2016-11-21 04:32:26 +0000

    more cleaning of results

--- team07/application/view/proto/results.php
+++ team07/application/view/proto/results.php
@@ -31,11 +31,11 @@
 
 
 
-    <div class="row" style="height:inherit">
+    <div class="row" style="display:flex">
 
 	<!-- ========== NAV BAR BLOCK =========== -->
-	    <div class="col-md-3" id="fancy-border" style="height:100%;padding-top:0; margin-left:15px;">
-	    	<div class="left-navigation" style=" height:100%;color:#330033;background-color:#FFFFFF">
+	    <div class="col-md-3" id="fancy-border" style="padding-top:0; margin-left:15px;display:flex;">
+	    	<div class="left-navigation" style="color:#330033;background-color:#FFFFFF">
 			    <ul style="list-style-type: none;">
 				<li style="font:bold;font-size:25px"> Listings Type </li>	
 				<li><input type="checkbox">Apartments</li>
@@ -79,32 +79,29 @@
 	<!-- START LISTINGS DISPLAY --> 
     	<?php foreach ($apartments as $apartment) { ?>
         <div class="row" id="fancy-border">
-	    <div class="col-xs-6 col-sm-3" style="padding:5px">
+	    <div class="col-sm-3">
                 <?php if (isset($apartment->image)) { ?>
                     <?php echo '<img src="data:image/jpeg;base64, '.base64_encode($apartment->image).
                         '"max-height="300px" max-width="300px"/>' ?>
                 <?php } ?>
-            </div>
-	        <div class="col-xs-9" id="pill-shape"><h5>TITLE</h5></div>   
-            <!-- <div style="color:blue;"><?php if (isset($apartment->id)) echo htmlspecialchars($apartment->id, ENT_QUOTES, 'UTF-8'); ?></div> --> 
-		<div class="col-xs-6 col-sm-2" id="pill-shape"><?php if (isset($apartment->address)) echo htmlspecialchars($apartment->address, ENT_QUOTES, 'UTF-8'); ?> </div>
-		<div class="col-sm-1"></div>
-		<div class="col-xs-6 col-sm-2"  id="pill-shape">$<?php if (isset($apartment->price)) echo htmlspecialchars($apartment->price, ENT_QUOTES, 'UTF-8'); ?> </div>
-		<div class="col-sm-1"></div>
-		<div class="col-xs-6 col-sm-2" id="pill-shape">SQURE FEET</div>	    
-		<div class="col-sm-1"></div>
+ 	    </div>
 
-	    	<div class="row">
+	    <div class="col-sm-8">
+			<div class="col-xs-9" id="pill-shape"><h5>TITLE</h5></div>   
+		    <!-- <div style="color:blue;"><?php if (isset($apartment->id)) echo htmlspecialchars($apartment->id, ENT_QUOTES, 'UTF-8'); ?></div> --> 
+			<div class="col-xs-9" id="pill-shape"><?php if (isset($apartment->address)) echo htmlspecialchars($apartment->address, ENT_QUOTES, 'UTF-8'); ?> </div>
+			<div class="col-xs-6 col-sm-2"  id="pill-shape">$<?php if (isset($apartment->price)) echo htmlspecialchars($apartment->price, ENT_QUOTES, 'UTF-8'); ?> </div>
+			<div class="col-xs-6 col-sm-2" id="pill-shape">SQURE FEET</div>	    
 			<div class="col-sm-1"></div>
-			<div class="col-xs-6 col-sm-2" id="pill-shape"> BEDS </div>
+
+			<div class="col-sm-2" id="pill-shape"> BEDS </div>
 			<div class="col-sm-1"></div>
-			<div class="col-xs-6 col-sm-2"id="pill-shape"> BATHROOM </div>
-			
-			<div class="col-xs-7"></div>	
-			<div class="col-xs-6 col-sm-2"id="pill-shape"> EMAIL</div>
+			<div class="col-sm-2"id="pill-shape"> BATHROOM </div>
+			<div class="col-sm-9"></div>
+			<div class="col-xs-5"id="pill-shape"> EMAIL</div>
 			<div class="col-sm-1"></div>
-			<div class="col-xs-6 col-sm-2"id="pill-shape"> PHONE </div>
-		</div>   	
+			<div class="col-xs-5"id="pill-shape"> PHONE </div>
+	     </div>	
         </div>
 	<!-- END OF LISTINGS DISPLAY -->
     <?php } ?>
