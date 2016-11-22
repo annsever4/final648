dbc3424439a711b8cb194f90f9664e328ca33969
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-21 17:01:36 -0800

    added the logout_page to the controller

--- team07/application/controller/logout_page.php
+++ team07/application/controller/logout_page.php
@@ -6,6 +6,15 @@
  * Date: 11/21/16
  * Time: 4:51 PM
  */
+
+public function index()
+{
+    // load views
+    require APP . 'view/_templates/header.php';
+    require APP . 'view/logout_page/index.php';
+    require APP . 'view/_templates/footer.php';
+}
+
 class logout_page extends Controller
 {
     public function logoutRegisteredUser(){
