0cc795296bf2ab19cef6df0abfa65c5d811eec93
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-21 17:04:12 -0800

    fixed syntax error in logout_page.php

--- team07/application/controller/logout_page.php
+++ team07/application/controller/logout_page.php
@@ -1,5 +1,6 @@
 <?php
-
+class logout_page extends Controller
+{
 /**
  * Created by PhpStorm.
  * User: lijie
@@ -7,17 +8,16 @@
  * Time: 4:51 PM
  */
 
-public function index()
-{
+    public function index()
+    {
     // load views
     require APP . 'view/_templates/header.php';
     require APP . 'view/logout_page/index.php';
     require APP . 'view/_templates/footer.php';
-}
+    }
 
-class logout_page extends Controller
-{
-    public function logoutRegisteredUser(){
+    public function logoutRegisteredUser()
+    {
 
         session_destroy();
         $_SESSION['logged_in'] = false;
