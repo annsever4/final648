6c7fcbd960d65cdbf5d68fd0ccfa48b49aa400bb
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-27 13:15:14 -0800

    degbug with console log

--- team07/application/view/map/result.php
+++ team07/application/view/map/result.php
@@ -59,7 +59,7 @@ echo "
       function initMap() {
         var uluru = {lat: -25.363, lng: 131.044};
         console.log('I am here');
-        console.log(var);
+        
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
