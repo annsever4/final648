48f46c23f4157922834521c4c0b858ccefaaba73
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-26 17:19:13 -0800

    print out geocoder

--- team07/application/view/map/result.php
+++ team07/application/view/map/result.php
@@ -73,6 +73,7 @@
     function initialize () {
         console.log("I am here");
         geocoder = new google.maps.Geocoder();
+        console.log(geocoder);
         var latlng = new google.maps.LatLng(53.2948557, -6.139267399999994);
         var mapOptions = {
             zoom: 10,
