79db158965b2b8c4e5634bc316dadd6a60dd85b3
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-26 22:39:16 -0800

    adding async defer

--- team07/application/view/_templates/header.php
+++ team07/application/view/_templates/header.php
@@ -9,49 +9,14 @@
     <!-- JS -->
     <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
     <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCP0FlO1B2ZZC5srVzlzpnnPjgZy2GysrQ"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCP0FlO1B2ZZC5srVzlzpnnPjgZy2GysrQ&callback=initMap"></script>
     <!-- Latest compiled and minified CSS, from CDN -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- CSS -->
     <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
 
-    <script>
-        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var labelIndex = 0;
-        function initialize(){
 
-            var bangalore = { lat: 12.97, lng: 77.59 };
-            var map = new google.maps.Map(document.getElementById('gmap_canvas'), {
-                zoom: 12,
-                center: bangalore
-            });
-
-            // This event listener calls addMarker() when the map is clicked.
-            google.maps.event.addListener(map, 'click', function(event) {
-                addMarker(event.latLng, map);
-            });
-
-            // Add a marker at the center of the map.
-            addMarker(bangalore, map);
-
-
-        }
-
-        // Adds a marker to the map.
-        function addMarker(location, map) {
-            // Add the marker at the clicked location, and add the next-available label
-            // from the array of alphabetical characters.
-            var marker = new google.maps.Marker({
-                position: location,
-                label: labels[labelIndex++ % labels.length],
-                map: map
-            });
-        }
-
-        google.maps.event.addDomListener(window, 'load', initialize);
-
-    </script>
 
 </head>
 
--- team07/application/view/map/result.php
+++ team07/application/view/map/result.php
@@ -55,8 +55,23 @@
 
     <!-- google map will be shown here -->
     
-    <div id="gmap_canvas">Loading map...</div>
+    <div id="map">Loading map...</div>
     <div id='map-label'>Map shows approximate location.</div>
+
+<script>
+    function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+        var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    }
+
+</script>
 
 
 
