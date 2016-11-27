03d8b009156977547eb8c900725d0bbdf3575872
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-26 22:16:59 -0800

    try gmap

--- team07/application/view/map/result.php
+++ team07/application/view/map/result.php
@@ -70,27 +70,40 @@
 </body>
 
     <script>
-    function init_map(){
-        var myOptions = {
-            zoom: 14,
-            center: new google.maps.LatLng(53.2948557, -6.139267399999994),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-        map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
-        marker = new google.maps.Marker({
-            map:map,
-            position: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>)
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var labelIndex = 0;
+    function initialize(){
+
+        var bangalore = { lat: 12.97, lng: 77.59 };
+        var map = new google.maps.Map(document.getElementById('gmap_canvas'), {
+            zoom: 12,
+            center: bangalore
         });
-        infowindow = new google.maps.InfoWindow({
-            content: "<?php echo $formatted_address; ?>"
+
+        // This event listener calls addMarker() when the map is clicked.
+        google.maps.event.addListener(map, 'click', function(event) {
+            addMarker(event.latLng, map);
         });
-        google.maps.event.addListener(marker, "click", function () {
-            infowindow.open(map, marker);
-        });
-        infowindow.open(map, marker);
+
+        // Add a marker at the center of the map.
+        addMarker(bangalore, map);
+
 
     }
-    google.maps.event.addDomListener(window, 'load', init_map);
+
+        // Adds a marker to the map.
+        function addMarker(location, map) {
+            // Add the marker at the clicked location, and add the next-available label
+            // from the array of alphabetical characters.
+            var marker = new google.maps.Marker({
+                position: location,
+                label: labels[labelIndex++ % labels.length],
+                map: map
+            });
+        }
+
+        google.maps.event.addDomListener(window, 'load', initialize);
+
     </script>
 
 
