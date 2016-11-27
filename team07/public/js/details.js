91174ea902946d2d8b44494dbf7c364cbe076b86
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-26 21:31:14 -0800

    test

--- team07/public/js/details.js
+++ team07/public/js/details.js
@@ -6,21 +6,24 @@ $(function(){
     if($(".row")[0]) {
         $('.row').on('click', function () {
             var listing_id = $(this).attr('id');
+
             $.ajax({
                 url: url + "details.php",
                 type: "POST",
                 data: {listing_detail_id: listing_id},
 
                 success: function (response) {
-                    window.location = url + "details";
                     console.log($(this).attr('id'));
+                    //window.location = url + "details";
                 },
 
                 error: function () {
                     console.log("ajax detail error");
                 }
 
+
             })
+
         })
     } else {
         console.log("No Result Rows Visible on this Page");
