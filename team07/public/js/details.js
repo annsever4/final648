87557dcf3d6496217469bcc22760d289f768ac52
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-26 20:50:03 -0800

    changed error in javascript post

--- team07/public/js/details.js
+++ team07/public/js/details.js
@@ -2,17 +2,18 @@
  * Created by euphoric on 11/26/16.
  */
 $(function(){
-
+    // if there are results rows on a page it  adds the on click method required to call the details controller
     if($(".row")[0]) {
         $('.row').on('click', function () {
             var listing_id = $(this).attr('id');
             $.ajax({
-                url: url + "details",
+                url: url + "details.php",
                 type: "POST",
                 data: {listing_detail_id: listing_id},
 
                 success: function (response) {
                     window.location = url + "details";
+                    console.log($(this).attr('id'));
                 },
 
                 error: function () {
