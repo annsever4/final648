3fed9242f0fc8d5237d09a49e92d923d79de75c0
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-11-28 14:23:58 -0800

    stuff

--- team07/public/js/profile.js
+++ team07/public/js/profile.js
@@ -1,6 +1,6 @@
 /* pagination */
 
-(function() {
+$(function() {
 
     if($('.clickable_details_row')[0]) {
         $('.clickable_details_row').on('click', function () {
@@ -130,4 +130,4 @@ $.fn.pageMe = function(opts){
 
 $('#items').pageMe({pagerSelector:'#myPager',childSelector:'tr',showPrevNext:true,hidePageNumbers:false,perPage:5});
 /****/
-})();
+});
