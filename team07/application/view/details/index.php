fc029d2b85909ac24d671e40e1e8f17135a5bf8b
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-28 14:09:59 -0800

    moving the map

--- team07/application/view/details/index.php
+++ team07/application/view/details/index.php
@@ -55,8 +55,9 @@
             </ul>
             <p>TODO FIGURE OUT WHAT TO PUT HERE: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
         </div>
-        <div class="col-md-6">
-            <img class="img-responsive" src="http://placehold.it/700x450" alt="">
+        <div class="col-md-6" id = 'map' style= 'width:500px; height:500px;'>
+
+<!--            <img class="img-responsive" src="http://placehold.it/700x450" alt="">-->
         </div>
     </div>
     <!-- /.row -->
