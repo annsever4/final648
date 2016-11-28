73d10f136e5fd472b5e2f125309748a7e1ecd5da
Lijie Zhou <lijie@mail.sfsu.edu>
2016-11-27 16:33:13 -0800

    syntax

--- team07/application/view/map/result.php
+++ team07/application/view/map/result.php
@@ -44,7 +44,7 @@
             var lat = {$lati};
             var lng = {$longi}
             ';
-      <?>
+      ?>
       function initMap() {
         
         <!-- var uluru = {lat: -25.363, lng: 131.044}; -->
