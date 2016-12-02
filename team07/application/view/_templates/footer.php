298cd9114af8f454bf7ba822f0b1e52ec83b4be4
benson <hzheng@mail.sfsu.edu>
2016-12-02 04:42:56 +0000

    google analytics added

--- team07/application/view/_templates/footer.php
+++ team07/application/view/_templates/footer.php
@@ -31,6 +31,18 @@
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88370313-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
     <div class="container-fluid footer" id="main_background">
             <div class="row">
 	    	&copy; This site is developed by the Team 007 @ SFSU
