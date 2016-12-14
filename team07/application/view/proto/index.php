071072cbe31765fb9d8341d6ff99c0663ef06daf
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-12-14 11:28:32 -0800

    removed old code

--- team07/application/view/proto/index.php
+++ team07/application/view/proto/index.php
@@ -52,32 +52,6 @@
 
 
 
-
-
-
-<?php foreach ($apartments as $apartment) { ?>
-
-        <tr>
-            <td><?php if (isset($apartment->id)) echo htmlspecialchars($apartment->id, ENT_QUOTES, 'UTF-8'); ?></td>
-            <td><?php if (isset($apartment->address)) echo htmlspecialchars($apartment->address, ENT_QUOTES, 'UTF-8'); ?> </td>
-            <td><?php if (isset($apartment->price)) echo htmlspecialchars($apartment->price, ENT_QUOTES, 'UTF-8'); ?> </td>
-            <td height="200" width="200">
-                <?php if (isset($apartment->image)) { ?>
-                   <?php echo '<img src="data:image/jpeg;base64, '.base64_encode($apartment->image).
-                        '" height="150" width="150"/>' ?>
-                <?php } ?>
-            </td></div>
-	</tr>
-<?php } ?>
-                </tbody>
-                </table>
-        </div>
-
-           
-<?php } ?>
-
-
-
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
