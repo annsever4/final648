3fd76e036ae64f2fa3e02eeac93a13fed8033c8a
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-12-14 14:01:24 -0800

    added Recent Listings Header 5

--- team07/application/view/proto/index.php
+++ team07/application/view/proto/index.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" cro    ssorigin="anonymous">
 
 <div class = "container-fluid">
-    <div class = "row">
+    <div class = "row" style="padding-bottom: 20px;">
         <div class = "col-md-6 col-md-offset-3">
             <h2 class="center">Recent Listings</h2>
         </div>
