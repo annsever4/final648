dd4905febf733b3460e6c9c4403531bd6547e15d
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-12-14 10:03:58 -0800

    added script to handle pill highlighting

--- team07/application/view/_templates/header.php
+++ team07/application/view/_templates/header.php
@@ -110,6 +110,10 @@
 
                    <script> $(document).ready(function() {
 
+                       var url_parts = location.href.split('/');
+                           var url_parts = url_parts.pop();
+                           var new_url = url_parts.join('/');
+
                     $('.nav-pills a[href="'+location.href+'"]').parents('li').addClass('active');
 
                     }); </script>
