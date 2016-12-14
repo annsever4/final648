a24e0d85d66bf9781ed91db9239dacbc68f8658d
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-12-14 09:05:30 -0800

    moving the logo a little bit out of the margin 7%

--- team07/application/view/_templates/header.php
+++ team07/application/view/_templates/header.php
@@ -34,7 +34,7 @@
     <div class="row flex"> 
 	 <div class="col-md-12">
 	   <div class="row" id="flex"> 
-	    <div class="col-md-4" id="logo"><img id="logo" style="margin-left:5%;" src="https://mgtvsportzedge.files.wordpress.com/2014/11/sfsu-gators.png"></div>
+	    <div class="col-md-4" id="logo"><img id="logo" style="margin-left:7%;" src="https://mgtvsportzedge.files.wordpress.com/2014/11/sfsu-gators.png"></div>
 	   
 	    <div class="col-md-4 center">
     		<h1 class="text_color_white" id="gatorlodge" style="font-size:50px">
