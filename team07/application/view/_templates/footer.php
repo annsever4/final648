30ec88b65b11f157c9d99a5aa5c5455a79d3e0e8
lfiggins@mail.sfsu.edu <lfiggins@mail.sfsu.edu>
2016-12-16 12:15:53 -0800

    maybe i got it this tie

--- team07/application/view/_templates/footer.php
+++ team07/application/view/_templates/footer.php
@@ -14,7 +14,7 @@
 
 
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <\
 <!-- <script src="http://maps.google.com/maps/api/js?key=AIzaSyCP0FlO1B2ZZC5srVzlzpnnPjgZy2GysrQ&"></script> -->
 <!-- <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script> -->
     <!-- our JavaScript -->
